401737e0842440f76fd76abaf648a9842c9adb16
Rafael Aguilar <r.aguilar@thirdevelopers.com>
2016-11-13 23:44:49 -0300

    Commit issued to merge unrelated histories.

---
+++ views/jobs.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+
+from models.core_models import Job
+
+
+class JobsFull(Resource):
+    def get(self, job_id, deep=0):
+        job = Job.get_unique(job_id)
+        return {job_id: job.to_json(deep)}
+
+    def put(self, job_id):
+        job = Job.get_unique(job_id)
+        print(job)
+        # create new instance
+        return {job.id}
