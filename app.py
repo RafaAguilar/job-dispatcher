9913ef73483c6fe7ec8dd5f61c75382acb4984a4
Rafael Aguilar <r.aguilar@thirdevelopers.com>
2016-11-14 01:46:29 -0300

    Handling better the RESTful endpoints on jobs.
    Minor: add swagger specs and requirements

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_restful_swagger import swagger
 
 from database import db
 
 
 def create_app():
     app = Flask(__name__)
-    app = Flask(__name__)
-    api = Api(app)
     app.config.from_object(os.environ['APP_SETTINGS'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    api = swagger.docs(Api(app), apiVersion='0.1')
     db.init_app(app)
-    api = Api(app)
     set_views(api)
     return app
 
@@ -23,8 +21,10 @@ def set_views(api):
     """
     This mutation is only to avoid circular imports issues.
     """
-    from views.jobs import JobsFull
-    api.add_resource(JobsFull, '/jobs/<string:job_id>')
+    from views.jobs import JobsCRUD
+    from views.jobs import JobsList
+    api.add_resource(JobsList, '/jobs')
+    api.add_resource(JobsCRUD, '/jobs/<string:job_id>')
 
 if __name__ == '__main__':
     app = create_app()
--- views/jobs.py
+++ views/jobs.py
@@ -1,19 +1,80 @@
 from flask_restful import Resource
 from flask_restful import abort
+from flask_restful_swagger import swagger
 
 from models.core_models import Job
+from database import db
 
 
-class JobsFull(Resource):
-    def get(self, job_id, deep=0):
-        job, is_new = Job.get_unique(job_id)
+class BaseView(Resource):
+
+    def abort_if_does_not_exist(self, o_id):
+        o, is_new = self.__managed_class__.get_unique(o_id)
         if is_new:
-            Job.remove_from_cache(job)
-            abort(404, message="Job {} doesn't exist".format(job_id))
+            self.__managed_class__.remove_from_cache(o)
+            abort(404, message="Job {} doesn't exist".format(o_id))
+        return o
+
+    def pagination(self, page=1, step=10):
+        pass
+
+
+class JobsList(BaseView):
+    __managed_class__ = Job
+
+    "An Endpoint to manage Jobs"
+
+    @swagger.operation(
+        notes='This endpoint will list existing jobs',
+        responseClass=__managed_class__.__name__,
+        nickname='Jobs',
+        parameters=[
+            {
+                "name": "job_id",
+                "description": "Job Unique Identifier: is needed to seek for the job instance.",
+                "required": True,
+                "allowMultiple": False,
+                "dataType": 'UUID',
+                "paramType": "url"
+            }
+        ],
+        responseMessages=[
+            {
+                "code": 302,
+                "message": "Found. It should show a JSON Array with all the created jobs."
+            }
+        ]
+    )
+    def get(self):
+        return db.session.query(Job).all()
+
+
+class JobsCRUD(BaseView):
+    __managed_class__ = Job
+
+    "An endpoint to get a Job details"
+    @swagger.operation(
+        notes='This endpoint will handle the CRUD operation for jobs',
+        responseClass=__managed_class__.__name__,
+        nickname='Jobs',
+        parameters=[],
+        responseMessages=[
+            {
+                "code": 302,
+                "message": "Found. The URL of the found job should be in the Location header"
+            },
+            {
+                "code": 202,
+                "message": "Feature not implemented YET. It will be soon"
+            }
+        ]
+    )
+    def get(self, job_id, deep=0):
+        job = self.abort_if_does_not_exist(job_id)
         return {job_id: job.to_json(deep)}
 
+    def delete(self, job_id):
+        return abort(202, message="The API doesn't have the proper methods to delete objects yet, soon will have.")
+
     def put(self, job_id):
-        job = Job.get_unique(job_id)
-        print(job)
-        # create new instance
-        return {job.id}
+        return abort(202, message="The API doesn't have the proper methods to delete objects yet, soon will have.")
